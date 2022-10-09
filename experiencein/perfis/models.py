32fa0c378204c3511c8523b0c8237f6f6f9a7953
diego-card <diegobrgr@gmail.com>
2022-10-09 19:09:43 -0300

    Feito até módulo 6.3

--- experiencein/perfis/models.py
+++ experiencein/perfis/models.py
@@ -1,3 +1,4 @@
+from pyclbr import Class
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,10 @@ class Perfil(models.Model):
     email = models.CharField(max_length=255, null=False)     
     telefone = models.CharField(max_length=15, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
+
+    def convidar(self, perfil_convidado):
+        Convite(solicitante=self, convidado=perfil_convidado).save()
+
+class Convite(models.Model):
+    solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+    convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
