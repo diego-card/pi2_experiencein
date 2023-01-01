f6e534e2cde277af36db1c91d01c327ae2efd297
diego <diegobrgr@gmail.com>
2022-12-31 21:26:42 -0300

    Adicionando listagem de perfis e envio de convites

--- experiencein-frontend/src/App.js
+++ experiencein-frontend/src/App.js
@@ -1,9 +1,32 @@
+import { NavLink, useHistory } from 'react-router-dom';
 import './App.css';
 import Routes from './routes';
+import { logout } from './Services/utils';
 
 function App() {
+  const history = useHistory();
+  function handleLogout() {
+    logout();
+    history.push("/");
+  }
   return (
     <div className='App'>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
       <Routes />
     </div>
   );
--- experiencein-frontend/src/Pages/Profiles/index.js
+++ experiencein-frontend/src/Pages/Profiles/index.js
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { useEffect, useInsertionEffect, useState } from "react";
+import { api } from "../../Services/api";
+import "./Profiles.css";
 
 export default function Profiles() {
-  return <div>perfis</div>;
+  const [profiles, setProfiles] = useState();
+  const [currentProfile, setCurrentProfile] = useState();
+  const [message, setMessage] = useState();
+  const [currentInvitedProfile, setCurrentInvitedProfile] = useState();
+  const [invites, setInvites] = useState();
+  
+  useEffect(() => {
+    api.get("/perfil/")
+    .then((resp) => setCurrentProfile(resp.data))
+    .catch((error) => console.error(error));
+
+    api.get("/perfis/")
+    .then((resp) => setProfiles(resp.data))
+    .catch((error) => console.error(error));
+
+    api.get("/convites/")
+    .then((resp) => setInvites(resp.data))
+    .catch((error) => console.error(error));
+  }, []);
+
+  function invite(id) {
+    api.post(`/convites/convidar/${id}`)
+    .then((resp) => setMessage(resp.data.mensagem))
+    .catch((error) =>  console.error(error));
+
+    setCurrentInvitedProfile(id);
+  };
+
+  return (
+    <div className="profiles">
+      <div className="invite">
+        {
+          profiles?.map((profile) => 
+              profile.id === currentProfile?.id ? null : (
+                <div key={profile.id}>
+                  <div className="card">
+                    <h3>{profile.nome}</h3>
+                    <span>{profile.email}</span>
+                    {profile.pode_convidar ? (
+                      <button className="icon" title="convidar" onClick={() => invite(profile.id)}></button>
+                    ) : null}
+                  </div>
+                  {profile.id === currentInvitedProfile ? (
+                    <span className="message">{message}</span>
+                  ) : null}
+                </div>
+              )
+            )
+        }
+      </div>
+      <div className="invitations">
+        <h2>convites</h2>
+        {invites?.map((invite) => (
+            <div className="card">
+              <h3>{invite.nome}</h3>
+              <button>Aceitar</button>
+            </div>
+          ))}
+      </div>
+      {/*<div>
+        {currentProfile?.contatos.map((contact) => (
+            <div className="card">
+              <h3>{ contact.nome }</h3>
+              <span>{contact.email}</span>
+            </div>
+          ))}
+        </div>*/}
+    </div>
+  );
 }
